d860d9cb883861956b0151323268302f96e41e33
Wzs010429 <1119127845@qq.com>
2023-08-19 17:19:37 +1200

    Modify the function for propositions preprocessing for people based on cwa.

--- cwa_processing_people.py
+++ cwa_processing_people.py
@@ -2,8 +2,7 @@ import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.tag import pos_tag
 import re
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
+
 
 
 def extract_adjectives_in_conditional_sentence(input_string):
@@ -133,9 +132,11 @@ def extract_names_and_attributes(text):
             words = word_tokenize(sentence)
             name = "".join(extract_names(words))
             if name:
-                dic[name] = []
+                if name not in dic:
+                    dic[name] = []
                 adjectives = extract_adjectives(words, name)
                 dic[name].extend(adjectives)
+
     return dic
 
 def create_output_string(dct, word_list):
@@ -150,11 +151,8 @@ def create_output_string(dct, word_list):
 if __name__ == "__main__":
     sentences_str = "Dave is strong. Dave is big. Charlie is thin. Charlie is short. Anne is smart. Alan is rough. Alan is bad. If someone is not huge then they are rough. If someone is not poor then they are quiet. If someone is smart then they are wealthy. If someone is wealthy and not dull then they are nice. If someone is rough and not huge then they are sad. If someone is thin and short then they are dull. If someone is dull and not wealthy then they are bad. All quiet people are kind. "
     first_list, second_list = extract_words_from_sentences(sentences_str)
-    print("The first list：", first_list)
-    print("The second list：", second_list)
+    # print("The first list：", first_list)
+    # print("The second list：", second_list)
 
-    print(find_unique_elements(first_list, second_list))
-
-    print(extract_names_and_attributes(sentences_str))
-
-    print(create_output_string(extract_names_and_attributes(sentences_str), find_unique_elements(first_list, second_list)))
+    # call this py file to return a string base on cwa (People)
+    # print(create_output_string(extract_names_and_attributes(sentences_str), find_unique_elements(first_list, second_list)))
