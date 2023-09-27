735030f5ee183ccc6c5d8b4047b3bada41065e3d
Wzs010429 <1119127845@qq.com>
2023-09-28 10:59:16 +1300

    Create the function for propositions preprocessing for Animals based on cwa.

--- cwa_processing_animal.py
+++ cwa_processing_animal.py
@@ -1,76 +1,94 @@
-import nltk
-nltk.download('punkt')
-nltk.download('averaged_perceptron_tagger')
-
-from nltk.tokenize import word_tokenize, sent_tokenize
-from nltk.tag import pos_tag
+import json
 import re
 
+animal_name = ['the bald eagle', 'the tiger', 'the bear', 'the lion', 'the wolf', 'the crocodile', 'the dinosaur',
+               'the snake', 'the leopard']
+animal_name_1 = ['the cat', 'the dog', 'the mouse', 'the rabbit', 'the squirrel']
+animal_relations = ['is', 'is not']
+animal_relations_1 = ['likes', 'chases', 'needs', 'visits', 'attacks', 'sees']
+# animal_relations_1_1 = {'does not like', 'does not chase', 'does not need', 'does not visit', 'does not eat'}
+animal_attributes_1 = ['kind', 'quiet', 'round', 'nice', 'smart']
+animal_attributes_2 = ['dull', 'rough', 'lazy', 'slow', 'sleepy']
+
+animal_attributes_3 = ['furry', 'small', 'cute', 'lovely', 'beautiful']
+animal_attributes_4 = ['big', 'strong', 'awful', 'fierce', 'heavy']
 
 
-def extract_adjectives_in_conditional_sentence(input_string):
-    words = input_string.split()
-    if "and" in words:
-        words.remove("and")
-    return words
+def split_sentences(text):
+    return re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
 
-def extract_words_from_sentences(sentences_str):
-    sentences = sentences_str.split(".")
-    print(sentences)
-    first_is_list = []
-    second_is_list = []
+animal_names = animal_name + animal_name_1
+animal_attributes = animal_attributes_1 + animal_attributes_2 + animal_attributes_3 + animal_attributes_4
+animal_relations = animal_relations + animal_relations_1
 
+
+def sentence_processing(text):
+    # 初始化输出字典和两个空列表
+    facts = {}
+    rules_condition = []  # 存储规则的条件部分（if部分）
+    rules_consequence = []  # 存储规则的结论部分（then部分）
+
+    # 分割文本为句子列表
+    sentences = split_sentences(text)
+
+    # 遍历每个句子并进行处理
     for sentence in sentences:
-        sentence = sentence.strip()
-        if "If" in sentence and "is" in sentence and "then" in sentence and "are" in sentence:
-            first_is_index = sentence.find("is")
-            if first_is_index != -1 and "then" in sentence:
-                first_word = sentence[first_is_index + 3:sentence.find("then")].strip()
-                if first_word:
-                    adjectives = extract_adjectives_in_conditional_sentence(first_word)
-                    first_is_list.extend(adjectives)
+        # 对于 "动物1 is 属性" 句型
+        if any(animal in sentence for animal in animal_names) and " is " in sentence and any(
+                attr in sentence for attr in animal_attributes) and " is not " not in sentence and "If" not in sentence:
+            parts = sentence.split(" is ")
+            animal = parts[0].strip()
+            attribute = parts[1].replace(".", "").strip()
+            if animal not in facts:
+                facts[animal] = []
+            facts[animal].append(attribute)
 
-                second_is_index = sentence[first_is_index+3:].find("are")
-                if second_is_index != -1:
-                    second_word = sentence[first_is_index+3+second_is_index+4:].strip()
-                    if second_word:
-                        second_is_list.append(second_word)
 
-        elif "All" in sentence and "people are" in sentence:
-            people_are_index = sentence.find("people are")
-            if people_are_index != -1:
-                first_word = sentence[3:people_are_index].strip()
-                if first_word:
-                    adjectives = extract_adjectives_in_conditional_sentence(first_word)
-                    first_is_list.extend(adjectives)
+        # 对于 "If something is 属性1 then it is 属性2" 句型
+        elif "If" in sentence and "then" in sentence and " is not " not in sentence and " and not " not in sentence and " and " not in sentence:
+            parts = sentence.split(" then ")
+            condition = parts[0].replace("If ", "").split(" is ")[1].strip()  # 获取属性1
+            consequence = parts[1].replace("it is ", "").replace(".", "").strip()  # 获取属性2
 
-                second_word = sentence[people_are_index + 10:].strip()
-                if second_word:
-                    second_is_list.append(second_word)
+            # 将属性添加到对应的列表中
+            rules_condition.append(condition)
+            rules_consequence.append(consequence)
 
-        elif "people are" in sentence:
-            people_are_index = sentence.find("people are")
-            if people_are_index != -1:
-                first_word = sentence[:people_are_index].strip().lower()
-                if first_word:
-                    adjectives = extract_adjectives_in_conditional_sentence(first_word)
-                    first_is_list.extend(adjectives)
+        # 对于 "If something is not 属性1 then it is 属性2" 句型
+        elif "If" in sentence and "then" in sentence and " is not " in sentence and " and not " not in sentence and " and " not in sentence:
+            parts = sentence.split(" then ")
+            condition = parts[0].replace("If ", "").split(" is not ")[1].split(" and ")[0].strip()  # 获取属性1
+            consequence = parts[1].replace("it is ", "").replace(".", "").strip()  # 获取属性2
 
-                second_word = sentence[people_are_index + 10:].strip()
-                if second_word:
-                    second_is_list.append(second_word)
+            # 将属性添加到对应的列表中
+            rules_condition.append(condition)
+            rules_consequence.append(consequence)
 
-    while "not" in first_is_list:
-        unique_set = set(first_is_list)
-        unique_set.discard("not")
-        first_is_list = list(unique_set)
+        # 句型 "If something is 属性1 and not 属性2 then it is 属性3"
+        elif "if" in sentence.lower() and "and not" in sentence and "then" in sentence and sum(
+                attribute in sentence for attribute in animal_attributes) > 2:
+            attributes_in_sentence = [attribute for attribute in animal_attributes if attribute in sentence]
+            if len(attributes_in_sentence) == 3:
+                rules_condition.extend(attributes_in_sentence[:2])
+                rules_consequence.append(attributes_in_sentence[2])
 
-    while "not" in second_is_list:
-        unique_set = set(second_is_list)
-        unique_set.discard("not")
-        second_is_list = list(unique_set)
+        # 句型 "If something is 属性1 and 属性2 then it is 属性3"
+        elif "if" in sentence.lower() and "and" in sentence and "then" in sentence and sum(
+                attribute in sentence for attribute in animal_attributes) > 2:
+            attributes_in_sentence = [attribute for attribute in animal_attributes if attribute in sentence]
+            if len(attributes_in_sentence) == 3:
+                rules_condition.extend(attributes_in_sentence[:2])
+                rules_consequence.append(attributes_in_sentence[2])
 
-    return first_is_list, second_is_list
+        # 句型 "All 属性1 animals are 属性2"
+        elif "all" in sentence.lower() and "animals are" in sentence and sum(
+                attribute in sentence for attribute in animal_attributes) == 2:
+            attributes_in_sentence = [attribute for attribute in animal_attributes if attribute in sentence]
+            if len(attributes_in_sentence) == 2:
+                rules_condition.append(attributes_in_sentence[0])
+                rules_consequence.append(attributes_in_sentence[1])
+
+    return facts, rules_condition, rules_consequence
 
 
 def find_unique_elements(list1, list2):
@@ -83,91 +101,19 @@ def find_unique_elements(list1, list2):
     return unique_elements
 
 
-
-def extract_adjectives(words, target_name):
-    tagged_words = pos_tag(words)
-
-    adjectives = []
-    name_found = False
-    verb_found = False
-
-    for word, tag in tagged_words:
-        if tag == 'NNP' and word == target_name:
-            name_found = True
-        elif name_found and not verb_found and tag.startswith('VB'):
-            verb_found = True
-        elif verb_found and tag.startswith('JJ'):
-            adjectives.append(word)
-        else:
-            name_found = False
-            verb_found = False
-
-    return adjectives
-
-def extract_names(words):
-    tagged_words = pos_tag(words)
-
-    names = []
-    current_name = []
-
-    for word, tag in tagged_words:
-        if tag == 'NNP':
-            current_name.append(word)
-        elif current_name:
-            names.append(" ".join(current_name))
-            current_name = []
-
-    if current_name:
-        names.append(" ".join(current_name))
-
-    return names
-
-def is_subject_verb_adjective(sentence):
-    pattern = r"^(NNP\s)*(VBZ\s)"
-    tagged_sentence = " ".join(tag for word, tag in sentence)
-    return re.match(pattern, tagged_sentence)
-
-def extract_names_and_attributes(text):
-    sentences = sent_tokenize(text)
-    dic = {}
-    for sentence in sentences:
-        if is_subject_verb_adjective(pos_tag(word_tokenize(sentence))):
-            words = word_tokenize(sentence)
-            name = "".join(extract_names(words))
-            if name:
-                if name not in dic:
-                    dic[name] = []
-                adjectives = extract_adjectives(words, name)
-                dic[name].extend(adjectives)
-
-    return dic
-
-def create_output_string(dct, word_list):
-    output_string = ""
-    for name, attributes in dct.items():
-        for word in word_list:
-            if word not in attributes:
-                output_string += f"{name} is not {word}. "
-    return output_string.strip()
-
-
 if __name__ == "__main__":
-    sentences_str = "Anne is huge. Anne is big. Anne is high. Harry is little. Harry is short. Alan is nice. Alan is quiet. Alan is kind. Erin is rough. Erin is poor. Erin is sad. Huge people are nice. If someone is little and short then they are small. If someone is rough and poor then they are dull. If someone is nice and quiet then they are wealthy. All small people are thin. All nice people are quiet. All wealthy people are smart. All dull people are bad."
-    first_list, second_list = extract_words_from_sentences(sentences_str)
-    print("The first list：", first_list)
-    print("The second list：", second_list)
-    print(extract_names_and_attributes(sentences_str))
-    print(find_unique_elements(first_list, second_list))
+    sentences_str = "the cat is furry. the dog is kind. the cat likes the dog. If something is lovely then it is kind. If something is not small then it is strong. If something is kind and not quiet then it is round. If something is cute and lovely then it is beautiful. All awful animals are heavy."
+    facts, rules_condition, rules_consequence = sentence_processing(sentences_str)
+    print("The first list：", rules_condition)
+    print("The second list：", rules_consequence)
 
     sentences = ""
 
-    for name in extract_names_and_attributes(sentences_str).keys():
+    for name in facts.keys():
         # for each words in the list
-        for word in find_unique_elements(first_list, second_list):
+        for word in find_unique_elements(rules_condition, rules_consequence):
             # Generate a sentence if the word is not in the value of the current key of the dictionary
-            if word not in extract_names_and_attributes(sentences_str)[name]:
+            if word not in facts[name]:
                 sentences = sentences + (f" {name} is not {word}.")
 
     print(sentences)
-    # call this py file to return a string base on cwa (People)
-    # print(create_output_string(extract_names_and_attributes(sentences_str), find_unique_elements(first_list, second_list)))
