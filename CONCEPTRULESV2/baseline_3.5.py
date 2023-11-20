4b52ea490a7e20c5c3e98e82381921aaff37b883
Wzs010429 <1119127845@qq.com>
2023-11-20 13:56:17 +1300

    Add Baseline for CONCEPTURERULES V1 and V2

---
+++ CONCEPTRULESV2/baseline_3.5.py
@@ -0,0 +1,59 @@
+import openai
+import json
+import csv
+import re
+import os
+import random
+
+def ai_function_generation(demo, context, question, model="gpt-3.5-turbo"):
+    messages = [{"role": "system", "content": demo},
+                {"role": "user", "content": f"Propositions: ```{context}```\nQuestion: ```{question}```"}]
+
+    response = openai.ChatCompletion.create(
+        model=model,
+        messages=messages,
+        temperature=0
+    )
+
+    return response.choices[0].message["content"]
+
+def remove_spaces(text):
+    text = re.sub(r' +', ' ', text)
+    text = re.sub(r'^ +| +$', '', text, flags=re.MULTILINE)
+    return text
+
+template = {
+    "ConceptRules_baseline": remove_spaces("""
+    Please help me complete a multi-step logical reasoning task. 
+    Please help me answer whether the question is correct or not based on the facts and rules formed by these natural language propositions. 
+    You should just return me one number as the final answer (1 for true and 0 for wrong) and also provide reasoning process. 
+    The Propositions and Questions are as follows: \n""")
+}
+
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def Baseline_ChatGPT_call(demo, context, question, model="gpt-3.5-turbo"):
+    return ai_function_generation(demo, context, question, model)
+
+# Read the JSON Lines file
+jsonl_file = "ConceptRulesV2/conceptrules_v2_full_train.jsonl"
+
+all_entries = []
+with open(jsonl_file, "r", encoding="utf-8") as file:
+    for line in file:
+        all_entries.append(json.loads(line))
+
+# Randomly select 100 entries from the loaded data
+selected_entries = random.sample(all_entries, 100)
+
+with open("ConceptRulesV2.csv", "w", newline="", encoding="utf-8") as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["question_id", "response", "label"])  # Write header
+
+    for entry in selected_entries:
+        context = entry["context"]
+        for question in entry["questions"]:
+            question_text = question["text"]
+            label = question["label"]
+            response = Baseline_ChatGPT_call(template['ConceptRules_baseline'], context, question_text)
+            csv_writer.writerow([question["id"], response, label])
---
+++ CONCEPTRULESV2/baseline_Llama2-7B.py
@@ -0,0 +1,75 @@
+from transformers import AutoTokenizer
+import transformers
+import torch
+import re
+import json
+import csv
+
+model = "meta-llama/Llama-2-7b-chat-hf"
+
+def remove_spaces(text):
+    # Replace multiple spaces with a single space
+    text = re.sub(r' +', ' ', text)
+    # Remove leading and trailing spaces from each line
+    text = re.sub(r'^ +| +$', '', text, flags=re.MULTILINE)
+    return text
+
+template = {
+    "ConceptRules_baseline": remove_spaces("""
+    Please help me complete a multi-step logical reasoning task. 
+    Please help me answer whether the question is correct or not based on the facts and rules formed by these natural language propositions. 
+    You should just return me one number as the final answer (1 for true and 0 for wrong) and also provide reasoning process. 
+    The Propositions and Questions are as follows: \n""")
+}
+
+tokenizer = AutoTokenizer.from_pretrained(model)
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+
+
+def batch_process(text):
+    sequences = pipeline(
+        text,
+        do_sample=True,
+        top_k=10,
+        num_return_sequences=1,
+        eos_token_id=tokenizer.eos_token_id,
+        max_length=2048,
+    )
+    return sequences[0]['generated_text']
+
+
+# List of json file names
+json_files = [
+    "../PARARULE_plus_step2_Animal_sample.json",
+    "../PARARULE_plus_step3_Animal_sample.json",
+    "../PARARULE_plus_step4_Animal_sample.json",
+    "../PARARULE_plus_step5_Animal_sample.json",
+    "../PARARULE_plus_step2_People_sample.json",
+    "../PARARULE_plus_step3_People_sample.json",
+    "../PARARULE_plus_step4_People_sample.json",
+    "../PARARULE_plus_step5_People_sample.json"
+]
+
+# Open the CSV file for writing
+with open("Llama2-7B.csv", "w", newline="", encoding="utf-8") as csv_file:
+    csv_writer = csv.writer(csv_file)
+    csv_writer.writerow(["step", "return", "label"])  # Write header
+
+    for json_file in json_files:
+        step = '_'.join(json_file.split("_")[2:4])
+        with open(json_file, "r", encoding="utf-8") as f:
+            data = json.load(f)
+            for entry in data:
+                context = entry["context"]
+                question = entry["question"]
+                label = entry["label"]
+                # Replace this with your actual function call
+                responses = batch_process(f"Instructions: ```{template['Llama2_baseline']}```Propositions: ```{context}```\nQuestion: ```{question}```")
+
+                csv_writer.writerow([step, responses, label])
+
